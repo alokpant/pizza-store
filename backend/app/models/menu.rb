2cf53f19620247336f7c4a020e19adcd22a7c697
Alok <justtodesign@gmail.com>
2023-11-16 11:50:59 +0100

    Update menu modal to include scopes and write specs for it

--- backend/app/models/menu.rb
+++ backend/app/models/menu.rb
@@ -6,6 +6,24 @@
 #  price             :float
 
 class Menu < ApplicationRecord
+  # Validations
   validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 20 }
   validates :price, presence: true, comparison: { greater_than_or_equal_to: 0 }
+
+  # Scopes
+  scope :search_by_name, ->(query) {
+    where('LOWER(name) LIKE ?', "%#{query.downcase}%")
+  }
+  scope :ordered_by, ->(field = :name, order_by = :asc) {
+    field ||= :name
+    order_by ||= :asc
+    valid_order_values = [:asc, :desc]
+    order_direction = valid_order_values.include?(order_by.to_sym) ? order_by.to_sym : :asc
+
+    if column_names.include?(field.to_s)
+      order("#{field} #{order_direction}")
+    else
+      raise ArgumentError, "Invalid field for ordering: #{field}"
+    end
+  }
 end
--- backend/test/models/menu_test.rb
+++ backend/test/models/menu_test.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 
 class MenuTest < ActiveSupport::TestCase
+  # Validations - start
   test 'name should be present' do
     menu = Menu.new(price: 10.99)
     assert_not menu.save, 'Saved menu without a name'
@@ -31,4 +32,56 @@ class MenuTest < ActiveSupport::TestCase
     menu = Menu.new(name: 'New Menu', price: -5.99)
     assert_not menu.save, 'Saved menu with a negative price'
   end
+  # Validations - end
+
+  # Scopes - start
+  test 'ordered_by should return data should order correctly by name' do
+    menus = Menu.ordered_by('name')
+    assert_equal ['Buffalo Ranch Chicken', 'Pesto Chicken', 'Supreme'], menus.pluck(:name)
+  end
+
+  test 'ordered_by should return data should order by name in descending order when provided' do
+    menus = Menu.ordered_by('name', :desc)
+    assert_equal ['Supreme', 'Pesto Chicken', 'Buffalo Ranch Chicken'], menus.pluck(:name)
+  end
+
+  test 'ordered_by should return data should order correctly by price' do
+    menus = Menu.ordered_by('price')
+    assert_equal ["Supreme", "Buffalo Ranch Chicken", "Pesto Chicken"], menus.pluck(:name)
+  end
+
+  test 'ordered_by should return data should order by price in descending order when provided' do
+    menus = Menu.ordered_by('price', :desc)
+    assert_equal ['Pesto Chicken', 'Buffalo Ranch Chicken', 'Supreme'], menus.pluck(:name)
+  end
+
+  test 'ordered_by should return raise ArgumentError when field is invalid' do
+    assert_raises ArgumentError do
+      Menu.ordered_by(:invalid_field)
+    end
+  end
+
+  test 'ordered_by should return order data by asc when invalid order_by is passed' do
+    menus = Menu.ordered_by('name', :invalid_order)
+    assert_equal ['Buffalo Ranch Chicken', 'Pesto Chicken', 'Supreme'], menus.pluck(:name)
+
+    menus = Menu.ordered_by('price', :invalid_order)
+    assert_equal ["Supreme", "Buffalo Ranch Chicken", "Pesto Chicken"], menus.pluck(:name)
+  end
+
+  test 'search_by_name should return data correctly when valid name is passed' do
+    menus = Menu.search_by_name('supreme')
+    assert_equal ['Supreme'], menus.pluck(:name)
+  end
+
+  test 'search_by_name should return empty array when invalid name is passed' do
+    menus = Menu.search_by_name('name')
+    assert_equal [], menus
+  end
+
+  test 'search_by_name should return multiple menus when passed' do
+    menus = Menu.search_by_name('chicken')
+    assert_equal ['Pesto Chicken', 'Buffalo Ranch Chicken'], menus.pluck(:name)
+  end
+  # Scopes - end
 end
